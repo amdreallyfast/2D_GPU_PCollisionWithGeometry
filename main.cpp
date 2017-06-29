c9224ff7b212720670609a925ebd4fbed0b796b7
amdreallyfast <amdreallyfast@hotmail.com>
2017-06-28 19:16:44 -0600

    More cleanup, and now have both particle-particle collision and particle-geometry collision running
    The frame rate dropped by ~1/2 to ~30fps on my GTX 560M with both of
    them running instead of just one of them.  I don't know why yet.  It may
    simply be my old video card being pushed to the limit, or maybe it's
    that my particle-geometry collision detection and resolution is a crude
    loop.  I will perform optimizations later.

--- main.cpp
+++ main.cpp
@@ -216,8 +216,8 @@ void Init()
     // for moving particles
     particleUpdater = std::make_shared<ShaderControllers::ParticleUpdate>(particleBuffer);
 
-//    // for sorting, detecting collisions between, and resolving said collisions between particles
-//    particleCollisions = std::make_shared<ShaderControllers::ParticleCollisions>(particleBuffer, particlePropertiesBuffer);
+    // for sorting, detecting collisions between, and resolving said collisions between particles
+    particleCollisions = std::make_shared<ShaderControllers::ParticleCollisions>(particleBuffer, particlePropertiesBuffer);
 
     // for drawing particles
     particleRenderer = std::make_shared<ShaderControllers::RenderParticles>();
@@ -250,7 +250,7 @@ void UpdateAllTheThings()
 
     particleResetter->ResetParticles(4);
     particleUpdater->Update(deltaTimeSec);
-    //particleCollisions->DetectAndResolve(false, false);
+    particleCollisions->DetectAndResolve(false, false);
     particleGeometryCollisions->DetectAndResolve(false);
 
 
